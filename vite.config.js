dc5a554a1be80b785a2d887b70d2c4ef92b3b7a9
nandmonlinellc <nandmonlinellc@gmail.com>
2025-08-26 08:52:25 -0400

    chore: configure modern build and legacy output

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import viteCompression from 'vite-plugin-compression';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -25,6 +26,11 @@ export default defineConfig({
       jpeg: { quality: 80 },
       jpg: { quality: 80 },
       webp: { quality: 80 },
+    }),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+      modernPolyfills: false,
+      renderLegacyChunks: true,
     }),
   ],
   resolve: {
